e335b4641a1cc9dfe8bb1b2f8dc2cdbd963d8a47
Vladislav <vladik.morozov.2012@mail.ru>
2020-11-19 23:43:27 +1000

    Added function delete edge from tool

--- Assets/Player/Scripts/PlayerTools/EdgeCreaterTool.cs
+++ Assets/Player/Scripts/PlayerTools/EdgeCreaterTool.cs
@@ -26,7 +26,14 @@ namespace PlayerInputControls
     [RequireComponent(typeof(LaserPointer))]
     public class EdgeCreaterTool : PlayerTool
     {
-        private const string _grabActionName = "SelectVertex";
+        private enum State
+        {
+            None,
+            Selecting,
+        }
+
+        private const string _createEdgeActionName = "CreateEdgeAction";
+        private const string _deleteEdgeActionName = "DeleteEdgeAction";
         private const string _changeRangeActionName = "ChangeEdgeType";
         private const string _actionMapName = "EdgeCreaterActionMap";
 
@@ -34,20 +41,18 @@ namespace PlayerInputControls
         private float _rayCastRange = 1000;
         private InputActionMap _inputActions;
         private LaserPointer _laserPointer;
-        private Transform _transform;
-        private GameObject _gameObject;
         private List<Type> _edgeTypes;
         private int _typeIndex;
         private Vertex _firstVertex;
         private Vertex _secondVertex;
         private LinkParameters _linkParameters;
+        private State _state;
 
         private void Awake ()
         {
+            _state = State.None;
             _edgeTypes = new List<Type>();
             _laserPointer = GetComponent<LaserPointer>();
-            _transform = transform;
-            _gameObject = gameObject;
             _linkParameters = new LinkParameters(6, 6);
         }
 
@@ -66,24 +71,65 @@ namespace PlayerInputControls
 
         private void ChangeEdgeTypeAction_started (InputAction.CallbackContext obj) => ChangeIndex(Mathf.RoundToInt(obj.ReadValue<float>()));
 
-        private void SelectVertexAction_performed (InputAction.CallbackContext obj) => _firstVertex = RayCast(_rayCastRange).transform?.GetComponent<Vertex>();
+        private void SelectVertexAction_performed (InputAction.CallbackContext obj)
+        {
+            if (_state == State.None)
+            {
+                _firstVertex = RayCast(_rayCastRange).transform?.GetComponent<Vertex>();
+                _state = State.Selecting;
+            }
+        }
 
         private void SelectVertexAction_canceled (InputAction.CallbackContext obj)
         {
-            _secondVertex = RayCast(_rayCastRange).transform?.GetComponent<Vertex>();
-            if (_secondVertex != null)
-                _firstVertex?.Link(_secondVertex, _edgeTypes[_typeIndex], _linkParameters);
+            try
+            {
+                _secondVertex = RayCast(_rayCastRange).transform?.GetComponent<Vertex>();
+                if (_secondVertex != null)
+                    _firstVertex?.Link(_secondVertex, _edgeTypes[_typeIndex], _linkParameters);
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError(ex.Message);
+            }
+            finally
+            {
+                _state = State.None;
+            }
         }
+
+        private void DeleteEdgeAction_canceled (InputAction.CallbackContext obj)
+        {
+            try
+            {
+                _secondVertex = RayCast(_rayCastRange).transform?.GetComponent<Vertex>();
+                if (_secondVertex != null)
+                    _firstVertex?.UnLink(_secondVertex, _edgeTypes[_typeIndex]);
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError(ex.Message);
+            }
+            finally
+            {
+                _state = State.None;
+            }
+        }
+
 
         public override void RegisterEvents (IInputActionCollection inputActions)
         {
             _inputActions = new InputActionMap(_actionMapName);
-            var selectVertexAction = _inputActions.AddAction(_grabActionName, InputActionType.Button, "<Mouse>/leftButton");
+            var createEdgeAction = _inputActions.AddAction(_createEdgeActionName, InputActionType.Button, "<Mouse>/leftButton");
+            var deleteEdgeAction = _inputActions.AddAction(_deleteEdgeActionName, InputActionType.Button, "<Mouse>/rightButton");
             var changeEdgeTypeAction = _inputActions.AddAction(_changeRangeActionName, InputActionType.Button);
             changeEdgeTypeAction.AddCompositeBinding("1DAxis").With("Positive", "<Keyboard>/e").With("Negative", "<Keyboard>/q");
 
-            selectVertexAction.performed += SelectVertexAction_performed;
-            selectVertexAction.canceled += SelectVertexAction_canceled;
+            createEdgeAction.performed += SelectVertexAction_performed;
+            createEdgeAction.canceled += SelectVertexAction_canceled;
+
+            deleteEdgeAction.performed += SelectVertexAction_performed;
+            deleteEdgeAction.canceled += DeleteEdgeAction_canceled;
 
             changeEdgeTypeAction.started += ChangeEdgeTypeAction_started;
         }
