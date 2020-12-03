bcd296e52877452415dab8d2fc2a09122132e57d
Vladislav <vladik.morozov.2012@mail.ru>
2020-12-03 18:30:30 +1000

    Fixed the project name

--- Assets/Decembrists/Scripts/DecembristVertex.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Grpah3DVisualser
-{
-    public class DecembristVertex : Vertex
-    {
-        public bool IsDec { get; set; } = false;
-        public string Name { get; set; }
-    }
-}
