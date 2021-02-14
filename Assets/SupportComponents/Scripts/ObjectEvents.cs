19c2503775c8dc4cea4027beb5a54a8325a8be3c
Vladislav <vladik.morozov.2012@mail.ru>
2021-02-14 15:37:48 +1000

    Changed grpah to graph

--- Assets/SupportComponents/Scripts/ObjectEvents.cs
+++ Assets/SupportComponents/Scripts/ObjectEvents.cs
@@ -1,18 +1,18 @@
-﻿// This file is part of Grpah3DVisualizer.
+﻿// This file is part of Graph3DVisualizer.
 // Copyright © Gershuk Vladislav 2021.
 //
-// Grpah3DVisualizer is free software: you can redistribute it and/or modify
+// Graph3DVisualizer is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// Grpah3DVisualizer is distributed in the hope that it will be useful,
+// Graph3DVisualizer is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY, without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Grpah3DVisualizer.  If not, see <https://www.gnu.org/licenses/>.
+// along with Graph3DVisualizer.  If not, see <https://www.gnu.org/licenses/>.
 
 using System;
 using System.Collections;
