fc369084abccd91a4c938091c968428318a7f142
Vladislav <vladik.morozov.2012@mail.ru>
2021-03-28 14:27:28 +1000

    Updated GraphGenerator

--- Assets/Graph3D/Scripts/Graphs/GraphGenerator.cs
+++ Assets/Graph3D/Scripts/Graphs/GraphGenerator.cs
@@ -10,43 +10,58 @@ using UnityEngine;
 
 namespace Graph3DVisualizer.Graph3D
 {
-    public abstract class GraphGenerator
+    public abstract class AbstractGraphGenerator
     {
+        public abstract AbstractPlaceholder Placeholder { get; protected set; }
+
         public abstract void Generate (AbstractGraph abstractGraph);
     }
 
-    public class GridGenerator : GraphGenerator
+    public abstract class AbstractPlaceholder
     {
-        private const string _fontPath = "Font/CustomFontDroidSans-Bold";
+        public abstract IEnumerable<Vector3> GetPosition (int count);
+    }
+
+    public class GridPlaceholder : AbstractPlaceholder
+    {
+        public uint Dist { get; protected set; }
+        public Vector2Int GridDim { get; protected set; }
+
+        public GridPlaceholder (Vector2Int gridDim, uint dist) => (GridDim, Dist) = (gridDim, dist);
+
+        public override IEnumerable<Vector3> GetPosition (int count)
+        {
+            for (var i = 0; i < count; ++i)
+                yield return new Vector3(i % GridDim.x * Dist, i / GridDim.x % GridDim.y * Dist, i / (GridDim.x * GridDim.y) * Dist);
+        }
+    }
+
+    public class SimpleGenerator : AbstractGraphGenerator
+    {
+        private const string _fontPath = "Font/CustomFontArial";
         private const string _selectFrameTexture = "Textures/SelectFrame";
         private static readonly Font _customFont = Resources.Load<Font>(_fontPath);
         private static readonly TextTextureFactory _textTextureFactory = new TextTextureFactory(_customFont, 32);
 
-        public List<LinkInfo> Edges { get; protected set; }
+        public List<(int firstVertex, int secondVertex, Color color)> Edges { get; protected set; }
+        public override AbstractPlaceholder Placeholder { get; protected set; }
+        public List<(string text, Texture2D texture2D, string? id)> Vertexes { get; protected set; }
 
-        public Vector2 GridDim { get; protected set; }
-
-        public List<(string text, Texture2D texture2D)> Vertexes { get; protected set; }
-
-        public GridGenerator ()
-        {
-            Vertexes = new List<(string text, Texture2D texture2D)>();
-            Edges = new List<LinkInfo>();
-            GridDim = default;
-        }
-
-        public GridGenerator (List<(string text, Texture2D texture2D)> vertexes, List<LinkInfo> edges, Vector3 gridDim)
+        public SimpleGenerator (List<(string text, Texture2D texture2D, string? id)> vertexes, List<(int firstVertex, int secondVertex, Color color)> edges, AbstractPlaceholder placeholder)
         {
             Edges = edges ?? throw new ArgumentNullException(nameof(edges));
-            GridDim = gridDim;
             Vertexes = vertexes ?? throw new ArgumentNullException(nameof(vertexes));
+            Placeholder = placeholder ?? throw new ArgumentNullException(nameof(placeholder));
         }
 
         public override void Generate (AbstractGraph graphControler)
         {
+            using var coordsEnum = Placeholder.GetPosition(Vertexes.Count).GetEnumerator();
+            var createdVertexes = new SelectableVertex[Vertexes.Count];
+
             for (var i = 0; i < Vertexes.Count; ++i)
             {
-                (var text, var texture2D) = Vertexes[i];
+                var (text, texture2D, id) = Vertexes[i];
                 texture2D.name = "Target";
                 var selectFrame = Texture2DExtension.ResizeTexture(Resources.Load<Texture2D>(_selectFrameTexture), 200, 200);
                 selectFrame.name = "SelectFrame";
@@ -60,14 +75,14 @@ namespace Graph3DVisualizer.Graph3D
                 const float scale = 10;
                 var textParameters = new BillboardParameters(textTexture, new Vector4(0, -5, 0, 0), new Vector2(scale, textTexture.height * 1.0f / textTexture.width * scale));
 
-                graphControler.SpawnVertex<SelectableVertex, SelectableVertexParameters>(
-                    new SelectableVertexParameters(new[] { imageParameters, textParameters }, selectFrameParameters, new Vector3(i % GridDim.x * 20, i / GridDim.x % GridDim.y * 20, i / (GridDim.x * GridDim.y) * 20)));
+                createdVertexes[i] = graphControler.SpawnVertex<SelectableVertex, SelectableVertexParameters>(
+                    new SelectableVertexParameters(new[] { imageParameters, textParameters }, selectFrameParameters, coordsEnum.Current, id: id));
+
+                coordsEnum.MoveNext();
             }
 
-            foreach (var linkInfo in Edges)
-            {
-                graphControler.GetVertexById(linkInfo.FirstVertexId).Link(graphControler.GetVertexById(linkInfo.FirstVertexId), linkInfo.EdgeType, linkInfo.EdgeParameters);
-            }
+            foreach (var (firstVertex, secondVertex, color) in Edges)
+                createdVertexes[firstVertex].Link<StretchableEdge, StretchableEdgeParameters>(createdVertexes[secondVertex], new StretchableEdgeParameters(new StretchableEdgeMaterialParameters(color)));
         }
     }
 }
